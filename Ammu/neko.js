dfaad6002fafff15da3188ea14a8371bfa50f66b
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-05-30 23:09:56 +0530

    Update neko.js

--- Ammu/neko.js
+++ Ammu/neko.js
@@ -3,8 +3,8 @@ let handler = async (m, { conn }) => {
   let res = await fetch('https://api.waifu.pics/sfw/neko')
   if (!res.ok) throw eror
   let json = await res.json()
-  if (!json.url) throw 'Eror!'
-  conn.sendFile(m.chat, json.url, '', watermark, m, 0, { thumbnail: await (await fetch(json.url)).buffer() })
+  if (!json.url) throw 'Error!'
+  conn.sendFile(m.chat, json.url, '', 'ɴᴀɴᴅʜᴜᴛᴛʏ ᴠ3', m, 0, { thumbnail: await (await fetch(json.url)).buffer() })
 }
 handler.help = ['neko']
 handler.tags = ['internet']

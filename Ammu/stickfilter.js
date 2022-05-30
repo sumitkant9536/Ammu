da88fe227b002898ee807b1731b6624b4c16e84b
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-05-31 02:12:20 +0530

    Update stickfilter.js

--- Ammu/stickfilter.js
+++ Ammu/stickfilter.js
@@ -7,32 +7,33 @@ const effects = ['greyscale', 'invert', 'brightness', 'threshold', 'sepia', 'red
 let handler = async (m, { conn, usedPrefix, text, command }) => {
   let effect = text.trim().toLowerCase()
   if (!effects.includes(effect)) throw `
-┌─〔 List Effect 〕
+┌─「 *List of Effects * 」
 ${effects.map(effect => `├ ${effect}`).join('\n')}
 └────
-Example: 
+
+Use :
+${usedPrefix + command} <effect>
+
+Example :
 ${usedPrefix + command} greyscale
 `.trim()
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw 'reply to the picture!'
-  if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} not supported`
+  if (!mime) throw `Reply image with command *${usedPrefix + command}*`
+  if (!/image\/(jpe?g|png)/.test(mime)) throw `Media not supported!`
   let img = await q.download()
   let url = await uploadImage(img)
-  let apiUrl = global.API('https://some-random-api.ml/canvas/', encodeURIComponent(effect), {
+  let apiUrl = API('https://some-random-api.ml/canvas/', encodeURIComponent(effect), {
     avatar: url
   })
   try {
-    let stiker = await sticker(null, apiUrl, global.packname, global.author)
-    await conn.sendMessage(m.chat, stiker, MessageType.sticker, {
-      quoted: m
-    })
+    let stiker = await sticker(null, apiUrl, packname, author)
+    await conn.sendFile(m.chat, stiker, '', '', m, 0, { asSticker: true })
   } catch (e) {
     await conn.sendFile(m.chat, apiUrl, 'image.png', null, m, 0, { thumbnail: await (await fetch(apiUrl)).buffer() })
   }
 }
-
-handler.help = ['stikerfilter']
+handler.help = ['stickerfilter']
 handler.tags = ['sticker']
 handler.command = /^(s(tic?ker)?filter)$/i
 

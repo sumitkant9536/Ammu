90909435551b6d645f5c9bcb7474c7e78d38640c
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-05-30 21:34:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let fs = require('fs')
-global.owner = ['919656459062'] // Letakan nomor kamu disini
+global.owner = ['919758523260'] // Letakan nomor kamu disini
 global.mods = [] // Moderator?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 global.APIs = { // API Prefix
@@ -31,14 +31,14 @@ global.APIKeys = { // APIKey nya disini
 }
 
 // Sticker WM
-global.packname = 'Ammu-Sticker'
-global.author = 'Ammu'
+global.packname = 'Sirius-Sticker'
+global.author = 'Sirius'
 
 global.wait = '_*Please Wait..*_'
 global.eror = '_*Server Error*_'
 global.fla = 'https://ibb.co/jW31z7n'
 
-global.watermark = '© Ammu'
+global.watermark = '© Sirius'
 
 global.multiplier = 69 // Semakin tinggi, semakin sulit naik level
 

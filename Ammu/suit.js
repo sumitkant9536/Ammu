6a5d810198fd56ba6e0f42728f820cea50f00e1c
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-05-31 13:43:45 +0530

    Update suit.js

--- Ammu/suit.js
+++ Ammu/suit.js
@@ -21,7 +21,7 @@ Silahkan @${m.mentionedJid[0].split`@`[0]}
   let footer = `Type "accept/ok/gas" to start suit\nType "reject/cannot/later" to decline`
   conn.suit[id] = {
     chat: await conn.send2Button(m.chat, caption, footer, 'Accept', 'ok', 'Reject', 'tolak', m, { contextInfo: { mentionedJid: conn.parseMention(caption) } }),
-    id: id,
+    id:m.id,
     p: m.sender,
     p2: m.mentionedJid[0],
     status: 'wait',

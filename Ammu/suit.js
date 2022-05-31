1e1be7128232d5c2879f8d47ff88bc2c88769438
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-05-31 15:43:07 +0530

    Update suit.js

--- Ammu/suit.js
+++ Ammu/suit.js
@@ -1,41 +1,42 @@
 /* 
-    Made by https://github.com/syahrularranger 
-    Jangan di hapus credit nya :)
+    Modifikasi by https://github.com/unx21
+    tanpa button :)
 */
 let timeout = 60000
-let poin = 500
-let poin_lose = -100
+let poin = 1000
+let poin_lose = 500
 let handler = async (m, { conn, usedPrefix }) => {
   conn.suit = conn.suit ? conn.suit : {}
-  if (Object.values(conn.suit).find(room => room.id.startsWith('suit') && [room.p, room.p2].includes(m.sender))) throw 'Complete your previous suit'
-  if (!m.mentionedJid[0]) return m.reply(`_Who do you want to challenge?_\nTag the person.. Example\n\n${usedPrefix}suit @${owner[1]}`, m.chat, { contextInfo: { mentionedJid: [owner[1] + '@s.whatsapp.net'] } })
-  if (Object.values(conn.suit).find(room => room.id.startsWith('suit') && [room.p, room.p2].includes(m.mentionedJid[0]))) throw `The person you are challenging is playing suit with someone else :(`
+  if (Object.values(conn.suit).find(room => room.id.startsWith('suit') && [room.p, room.p2].includes(m.sender))) throw 'Selesaikan suit mu yang sebelumnya'
+  if (!m.mentionedJid[0]) return m.reply(`_Siapa yang ingin kamu tantang?_\nTag orangnya.. Contoh\n\n${usedPrefix}suit @${owner[0]}`, m.chat, { contextInfo: { mentionedJid: [owner[0] + '@s.whatsapp.net'] } })
+  if (Object.values(conn.suit).find(room => room.id.startsWith('suit') && [room.p, room.p2].includes(m.mentionedJid[0]))) throw `Orang yang kamu tantang sedang bermain suit bersama orang lain :(`
   let id = 'suit_' + new Date() * 1
   let caption = `
-_*SUIT PvP*_
+      *「 SUIT PVP 」*
 
-@${m.sender.split`@`[0]} challenging @${m.mentionedJid[0].split`@`[0]} to play suit
+@${m.sender.split`@`[0]} menantang @${m.mentionedJid[0].split`@`[0]} untuk bermain suit
 
-Silahkan @${m.mentionedJid[0].split`@`[0]} 
+Silahkan @${m.mentionedJid[0].split`@`[0]}
+
+Ketik "terima/ok/gas" untuk memulai suit\nKetik "tolak/gabisa/nanti" untuk menolak
 `.trim()
-  let footer = `Type "accept/ok/gas" to start suit\nType "reject/cannot/later" to decline`
   conn.suit[id] = {
-    chat: await conn.send2Button(m.chat, caption, footer, 'Accept', 'ok', 'Reject', 'tolak', m, { contextInfo: { mentionedJid: conn.parseMention(caption) } }),
-    id:m.id,
+    chat: await conn.reply(m.chat, caption, m, { contextInfo: { mentionedJid: conn.parseMention(caption) } }),
+    id: id,
     p: m.sender,
     p2: m.mentionedJid[0],
     status: 'wait',
     waktu: setTimeout(() => {
-      if (conn.suit[id]) conn.reply(m.chat, `_Suit time out_`, m)
+      if (conn.suit[id]) conn.reply(m.chat, `_Waktu suit habis_`, m)
       delete conn.suit[id]
     }, timeout), poin, poin_lose, timeout
   }
 }
-handler.tags = ['game']
-handler.help = ['suitpvp', 'suit'].map(v => v + ' @tag')
-handler.command = /^suit(pvp)?$/i
-
+handler.help = ['suitpvp <@user>', 'suit2 <@users>']
+handler.tags = ['games']
+handler.command = /^suit(pvp|2)$/i
+handler.limit = false
 handler.group = true
-handler.game = true
+handler.register = true
 
 module.exports = handler
